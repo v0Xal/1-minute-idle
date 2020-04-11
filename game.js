7848871a3588b020fe2507ff7008f92397e4cc8a
v0Xal <58456527+v0Xal@users.noreply.github.com>
2020-04-10 20:48:14 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 var game = {
 "money":0,
 "time":0,
+  "souls":0,
+  "soulFluid":0,
+  "soulFluidNeeded":10000,
 "maxTime":60,
 "clocks":0,
 "clockPower":1,
 "clockProgress":0
 }
-
+function val(id,thing){
+  document.getElementById(id).innerHTML = thing
+}
 
 function countdown(){
 if(game.time > 0){
@@ -17,8 +22,12 @@ endGame();
 }
 }
 var repeat = setInterval(countdown,1000);
-
-
+function update(){
+  val("soulFluidCounter",game.soulFluid + "/" + game.soulFluidNeeded + " Soul Fluid")
+  document.getElementById("soulFluid").style.height = game.soulFluid/game.soulFluidNeeded +"%";
+  val("souls",game.souls + " Souls");
+}
+setInterval(update,50);
 function startGame(){
 game.time = game.maxTime;
 game.money = 0;
